4e917d04e7a6dd8704ec12f52c4b36faa4910e51
rishabpuranika <145rishab@gmail.com>
2025-08-05 19:01:19 +0530

    working upload render

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def download():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
